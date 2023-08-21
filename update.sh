13fa694593aa36699cd012592aa765349e93b88e
Julien Mousqueton <julien@mousqueton.io>
2023-08-21 17:28:14 +0200

    Delete update.sh

--- update.sh
+++
@@ -1,6 +0,0 @@
-
-curl https://codeload.github.com/montysecurity/C2-Tracker/zip/refs/heads/main -o /tmp/tmp.zip
-unzip -j -o  /tmp/tmp.zip 'C2-Tracker-main/data/*' -d ./data
-git add . 
-git commit --all --message "Nightly Update"
-git push
