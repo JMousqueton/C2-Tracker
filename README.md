98107f488414beb036efa52ce9ec6b70e7361469
Julien <julien.mousqueton@computacenter.com>
2024-08-07 15:44:35 +0200

    Nightly Update

--- README.md
+++ README.md
@@ -1,10 +1,38 @@
 # C2 Tracker
 
-This repo is a modified fork of [Montysecurity C2-Tracker](https://github.com/montysecurity/C2-Tracker)
+C2 Tracker is a free-to-use-community-driven IOC feed that uses [Shodan](https://www.shodan.io/) and [Censys](https://search.censys.io/) searches to collect IP addresses of known malware/botnet/C2 infrastructure.
 
-C2-Tracker mines various C2/malware IPs from Shodan. Most of the searches used were sourced from [Michael Koczwara's](https://michaelkoczwara.medium.com/) and [@BushidoToken's (Will's)](https://twitter.com/BushidoToken) research (see references below). Huge thanks to the both of them!
+## Honorable Mentions
 
-## What is tracked?
+Many of the queries have been sourced from other CTI researchers:
+
+- [BushidoToken](https://twitter.com/BushidoToken)
+- [Michael Koczwara](https://twitter.com/MichalKoczwara)
+- [ViriBack](https://twitter.com/ViriBack)
+- [Gi7W0rm](https://twitter.com/Gi7w0rm)
+- [Glacius_](https://twitter.com/Glacius_)
+- [corumir](https://github.com/corumir)
+
+Huge shoutout to them!
+
+Thanks to [BertJanCyber](https://twitter.com/BertJanCyber) for creating the [KQL query](https://github.com/Bert-JanP/Hunting-Queries-Detection-Rules/blob/main/Threat%20Hunting/TI%20Feed%20-%20MontySecurity%20C2%20Tracker%20All%20IPs.md) for ingesting this feed
+
+And finally, thanks to [Y_nexro](https://twitter.com/Y_NeXRo) for creating [C2Live](https://github.com/YoNixNeXRo/C2Live) in order to visualize the data
+
+## Usage
+
+The most recent collection will be stored in `data/`. The IPs are seperated by the name of the tool and there is an `all.txt` that contains all of the IPs. As it currently stands this feed updates `weekly` on Monday.
+
+### Ingestion/Alerting
+
+- If your SIEM/EDR/TIP has the ability to ingest data from a remote source than you can use the files in their raw text format. See BertJanCyber's KQL query above as an example
+- FortinetSIEM 7.2.0 added support for this intel feed - `https://docs.fortinet.com/document/fortisiem/7.2.0/release-notes/553241/whats-new-in-7-2-0`
+
+### Investigations/Historical Analysis
+
+- The repo, by its nature, has version control. This means you can search the history of the repo for when an IP was present in the results. I have used one of my other public tools, [GitHub Repo OSINT Tool](https://github.com/montysecurity/GROT), for this purpose.
+
+## What do I track?
 
 - C2's
     - [Cobalt Strike](https://www.cobaltstrike.com/)
@@ -18,6 +46,18 @@ C2-Tracker mines various C2/malware IPs from Shodan. Most of the searches used w
     - PANDA
     - [NimPlant C2](https://github.com/chvancooten/NimPlant)
     - [Havoc C2](https://github.com/HavocFramework/Havoc)
+    - [Caldera](https://caldera.mitre.org/)
+    - [Empire](https://github.com/EmpireProject/Empire)
+    - [Ares](https://github.com/sweetsoftware/Ares)
+    - [Hak5 Cloud C2](https://shop.hak5.org/products/c2)
+    - [Pantegana](https://github.com/cassanof/pantegana)
+    - [Supershell](https://github.com/tdragon6/Supershell/tree/main)
+    - Poseidon C2
+    - Viper C2
+    - [UnamWebPanel](https://github.com/UnamSanctam/UnamWebPanel)
+    - [Vshell](https://github.com/veo/vshell)
+    - [Villain](https://github.com/t3l3machus/Villain)
+    - [Nimplant C2](https://github.com/chvancooten/NimPlant)
 - Malware
     - AcidRain Stealer
     - Misha Stealer (AKA Grand Misha)
@@ -25,35 +65,77 @@ C2-Tracker mines various C2/malware IPs from Shodan. Most of the searches used w
     - RAXNET Bitcoin Stealer
     - Titan Stealer
     - Collector Stealer
-    - Mystic Stealer
+    - [Mystic Stealer](https://twitter.com/_montysecurity/status/1643164749599834112)
+    - [Gotham Stealer](https://twitter.com/FalconFeedsio/status/1705765083429863720)
+    - [Meduza Stealer](https://twitter.com/g0njxa/status/1717563999984717991?t=rcVyVA2zwgJtHN5jz4wy7A&s=19)
+    - Quasar RAT
+    - ShadowPad
+    - AsyncRAT
+    - DcRat
+    - BitRAT
+    - DarkComet Trojan
+    - XtremeRAT Trojan
+    - NanoCore RAT Trojan
+    - Gh0st RAT Trojan
+    - DarkTrack RAT Trojan
+    - njRAT Trojan
+    - Remcos Pro RAT Trojan
+    - Poison Ivy Trojan
+    - Orcus RAT Trojan
+    - ZeroAccess Trojan
+    - HOOKBOT Trojan
+    - [RisePro Stealer](https://github.com/noke6262/RisePro-Stealer)
+    - NetBus Trojan
+    - Bandit Stealer
+    - Mint Stealer
+    - Mekotio Trojan
+    - Gozi Trojan
+    - Atlandida Stealer
+    - VenomRAT
+    - Orcus RAT
+    - DcRAT
+    - BitRAT
+    - BlackDolphin
+    - Artemis RAT
+    - Godzilla Loader
+    - Jinx Loader
+    - Netpune Loader
+    - [SpyAgent](https://www.deepinstinct.com/blog/the-russian-spyagent-a-decade-later-and-rat-tools-remain-at-risk)
 - Tools
-    - [Hashcat Cracking Tool](https://hashcat.net/hashcat/)
-    - [BurpSuite](https://portswigger.net/burp)
-    - [PowerSploit](https://github.com/PowerShellMafia/PowerSploit)
     - [XMRig Monero Cryptominer](https://xmrig.com/)
     - [GoPhish](https://getgophish.com/)
+    - [Browser Exploitation Framework (BeEF)](https://github.com/beefproject/beef)
+    - [BurpSuite](https://portswigger.net/burp)
+    - [Hashcat](https://hashcat.net/hashcat/)
+    - [MobSF](https://github.com/MobSF/Mobile-Security-Framework-MobSF)
+- Botnets
+    - [7777](https://gi7w0rm.medium.com/the-curious-case-of-the-7777-botnet-86e3464c3ffd)
+    - [BlackNET](https://github.com/suriya73/BlackNET)
+    - Doxerina
+    - Scarab
 
-## Current State
+## Running Locally
 
-This script uses GithubAction nightly to automatically update the files in `data`.
-
-### Running Locally
-
-However if you want to host a private version, put your Shodan API key in an environment variable called `SHODAN_API_KEY`
+If you want to host a private version, put your Shodan API key in an environment variable called `SHODAN_API_KEY`, and setup your Censys credentials in `CENSYS_API_ID` & `CENSYS_API_SECRET`
 
 ```bash
-echo SHODAN_API_KEY=API_KEY >> .env
-source .env
 python3 -m pip install -r requirements.txt
 python3 tracker.py
 ```
+
+## Contributing
+
+I encourage opening an issue/PR if you know of any additional Shodan/Censys searches for identifying adversary infrastructure. I will not set any hard guidelines around what can be submitted, just know, **fidelity is paramount** (high true/false positive ratio is the focus).
 
 ## References
 
 - [Hunting C2 with Shodan by Michael Koczwara](https://michaelkoczwara.medium.com/hunting-c2-with-shodan-223ca250d06f)
 - [Hunting Cobalt Strike C2 with Shodan by Michael Koczwara](https://michaelkoczwara.medium.com/cobalt-strike-c2-hunting-with-shodan-c448d501a6e2)
-- [This tweet](https://twitter.com/MichalKoczwara/status/1591750513238118401?cxt=HHwWgsDUiZGqhJcsAAAA)
+- [https://twitter.com/MichalKoczwara/status/1591750513238118401?cxt=HHwWgsDUiZGqhJcsAAAA](https://twitter.com/MichalKoczwara/status/1591750513238118401?cxt=HHwWgsDUiZGqhJcsAAAA)
 - BushidoToken's [OSINT-SearchOperators](https://github.com/BushidoUK/OSINT-SearchOperators/blob/main/ShodanAdversaryInfa.md)
-- [This tweet](https://twitter.com/MichalKoczwara/status/1641119242618650653)
-- [This tweet](https://twitter.com/MichalKoczwara/status/1641676761283850241)
-- [This tweet](https://twitter.com/_montysecurity/status/1643164749599834112)
+- [https://twitter.com/MichalKoczwara/status/1641119242618650653](https://twitter.com/MichalKoczwara/status/1641119242618650653)
+- [https://twitter.com/MichalKoczwara/status/1641676761283850241](https://twitter.com/MichalKoczwara/status/1641676761283850241)
+- [https://twitter.com/_montysecurity/status/1643164749599834112](https://twitter.com/_montysecurity/status/1643164749599834112)
+- [https://twitter.com/ViriBack/status/1713714868564394336](https://twitter.com/ViriBack/status/1713714868564394336)
+- [https://gi7w0rm.medium.com/the-curious-case-of-the-7777-botnet-86e3464c3ffd](https://gi7w0rm.medium.com/the-curious-case-of-the-7777-botnet-86e3464c3ffd)
+- [https://twitter.com/Glacius_/status/1731699013873799209](https://twitter.com/Glacius_/status/1731699013873799209)
--- tracker.py
+++ tracker.py
@@ -1,164 +1,447 @@
-#!/usr/bin/env python3
-"""
-Script Name: c2-tracker
-Description: Track C2 server using shodan queries
-"""
-
-# Import necessary modules
 import os
 from dotenv import load_dotenv
-from shodan import Shodan, APIError
-import logging
-
-
-# Logging 
-logging.basicConfig(
-    format='%(asctime)s,%(msecs)d %(levelname)-8s %(message)s',
-    datefmt='%Y-%m-%d:%H:%M:%S',
-    level=logging.INFO
-    )
-
-def stdlog(msg):
-    '''standard infologging'''
-    logging.info(msg)
-
-def errlog(msg):
-    '''standard error logging'''
-    logging.error(msg)
+from shodan import Shodan, exception
+from censys.search import CensysHosts
 
 def shodan():
     api_key = os.environ["SHODAN_API_KEY"].strip()
     api = Shodan(api_key)
-
-    try:
-        api_info = api.info()
-        remaining_queries = api_info["usage_limits"]["query_credits"]
-        query_credits = remaining_queries - api_info["scan_credits"]
-        if query_credits > 0:
-            stdlog("Shodan usage: "+ str(query_credits) + "/" + str(remaining_queries))
-        else:
-            errlog("No more Shodan credit ("+ str(query_credits) + "/" + str(remaining_queries)+")")
-            exit(1)
-    except APIError as error:
-        errlog("Error while retrieving API info: " + str(error))
-
     # https://michaelkoczwara.medium.com/hunting-c2-with-shodan-223ca250d06f
     # https://michaelkoczwara.medium.com/cobalt-strike-c2-hunting-with-shodan-c448d501a6e2
     # https://twitter.com/MichalKoczwara/status/1591750513238118401?cxt=HHwWgsDUiZGqhJcsAAAA
     # https://github.com/BushidoUK/OSINT-SearchOperators/blob/main/ShodanAdversaryInfa.md
     # https://twitter.com/MichalKoczwara/status/1641119242618650653
     # https://twitter.com/MichalKoczwara/status/1641676761283850241
-    # https://blog.projectdiscovery.io/hunting-c2-servers/
     queries = {
         "Cobalt Strike C2": [
             "ssl.cert.serial:146473198",
             "hash:-2007783223 port:50050",
-            "ssl.jarm:07d14d16d21d21d00042d41d00041de5fb3038104f457d92ba02e9311512c2",
-            "ssl.jarm:07d14d16d21d21d07c42d41d00041d24a458a375eef0c576d23a7bab9a9fb1+port:443",
             "product:'Cobalt Strike Beacon'",
-            "http.html:cs4.4",
             "ssl:foren.zik"
-            ],
+        ],
         "Metasploit Framework C2": [
             "ssl:MetasploitSelfSignedCA",
             "http.favicon.hash:-127886975",
-            "http.html:msf4"
-            ],
+            "product:Metasploit"
+        ],
         "Covenant C2": [
             "ssl:Covenant http.component:Blazor",
-            "http.favicon.hash:-737603591"
-            ],
+            "http.favicon.hash:-737603591",
+            "product:Covenant"
+        ],
         "Mythic C2": [
             "ssl:Mythic port:7443",
-            "http.favicon.hash:-859291042"
-            ],
+            "http.favicon.hash:-859291042",
+            "product:Mythic"
+        ],
         "Brute Ratel C4": [
             "http.html_hash:-1957161625",
             "product:'Brute Ratel C4'"
-            ],
-        "Posh C2": [ "ssl:P18055077" ],
-        "Sliver C2": [
-            "ssl:multiplayer ssl:operators",
-            "http.html:sliver-client",
-            '"HTTP/1.1 404 Not Found" "Cache-Control: no-store, no-cache, must-revalidate" "Content-Length: 0" -"Server:" -"Pragma:"'
-            ],
-        "Deimos C2": [ "http.html_hash:-14029177" ],
-        "PANDA C2":  [ "http.html:PANDA http.html:layui" ],
-        "NimPlant C2" : [
-            "Nimplant C2 Server",
-            "http.html_hash:-1258014549"
-            ],
-        "Havoc C2": [ "ssl:postalCode=3540 ssl.jarm:3fd21b20d00000021c43d21b21b43de0a012c76cf078b8d06f4620c2286f5e" ],
-        "GoPhish": [
-            "http.html:'Gophish - Login'",
-            "http.favicon.hash:803527991"
         ],
-        "AcidRain Stealer": [ 'http.html:"AcidRain Stealer"' ],
-        "Misha Stealer": [ "http.title:misha http.component:UIKit" ],
+        # https://x.com/pedrinazziM/status/1808629285726400879
+        "Posh C2": [ 
+            "ssl:P18055077",
+            "product:PoshC2",
+            "http.html_hash:855112502",
+            "http.html_hash:-1700067737"
+        ],
+        "Sliver C2": [
+            "ssl:multiplayer ssl.cert.issuer.cn:operators",
+            '"HTTP/1.1 404 Not Found" "Cache-Control: no-store, no-cache, must-revalidate" "Content-Length: 0" -"Server:" -"Pragma:"',
+            # https://twitter.com/Glacius_/status/1731699013873799209
+            "product:'Sliver C2'"
+        ],
+        "Deimos C2": [
+            "http.html_hash:-14029177",
+            "product:'Deimos C2'"
+        ],
+        "PANDA C2":  [
+            "http.html:PANDA http.html:layui",
+            "product:'Panda C2'"
+        ],
+        "NimPlant C2" : [
+            "http.html_hash:-1258014549"
+        ],
+        "Havoc C2": [
+            "X-Havoc: true",
+            "product:Havoc"
+        ],
+        # https://twitter.com/ViriBack/status/1713714868564394336
+        "Caldera C2": [
+            "http.favicon.hash:-636718605",
+            "http.html_hash:-1702274888",
+            'http.title:"Login | CALDERA"'
+        ],
+        "GoPhish": [
+            "http.title:'Gophish - Login'",
+        ],
+        "AcidRain Stealer": [
+            'http.html:"AcidRain Stealer"'
+        ],
+        "Misha Stealer": [
+            "http.title:misha http.component:UIKit"
+        ],
         "Patriot Stealer": [
             "http.favicon.hash:274603478",
             "http.html:patriotstealer"
         ],
-        "RAXNET Bitcoin Stealer": [ "http.favicon.hash:-1236243965" ],
-        "Titan Stealer": [ 'http.html:"Titan Stealer"' ],
-        "Hachcat Cracking Tool": [ "http.html:hashcat"],
+        "RAXNET Bitcoin Stealer": [
+            "http.favicon.hash:-1236243965"
+        ],
+        "Titan Stealer": [
+            "http.html:'Titan Stealer'"
+        ],
         "Collector Stealer": [
             'http.html:"Collector Stealer"',
             'http.html:getmineteam'
         ],
         "Mystic Stealer": [
-            "http.title:'Mystic Stealer v1.2 -  Login'",
+            "http.title:'Mystic Stealer'",
             "http.favicon.hash:-442056565"
         ],
-        "BurpSuite": [ "http.html:BurpSuite" ],
-        "PowerSploit" : [ "http.html:PowerSploit" ],
+        "Gotham Stealer": [
+            "http.title:'Gotham Stealer'",
+            "http.favicon.hash:-1651875345"
+        ],
+        # https://twitter.com/g0njxa/status/1717563999984717991?t=rcVyVA2zwgJtHN5jz4wy7A&s=19
+        "Meduza Stealer": [
+            "http.html_hash:1368396833",
+            "http.title:'Meduza Stealer'"
+        ],
         "XMRig Monero Cryptominer": [
             "http.html:XMRig",
             "http.favicon.hash:-782317534",
             "http.favicon.hash:1088998712"
         ],
-        "AsyncRat C2": [ "ssl.cert.issuer.cn:AsyncRAT"],
+        # https://gi7w0rm.medium.com/the-curious-case-of-the-7777-botnet-86e3464c3ffd
+        "7777 Botnet": [
+            "hash:1357418825"
+        ],
+        "Quasar RAT": [
+            "product:'Quasar RAT'"
+        ],
+        "ShadowPad" : [
+            "product:ShadowPad"
+        ],
+        "AsyncRAT": [
+            "product:AsyncRAT"
+        ],
+        "DcRAT": [
+            "product:DcRat"
+        ],
+        "BitRAT": [
+            "product:BitRAT"
+        ],
+        "Empire C2": [
+            "product:'Empire C2'"
+        ],
+        "DarkComet Trojan": [
+            "product:'DarkComet Trojan'"
+        ],
+        "XtremeRAT Trojan": [
+            "product:'XtremeRAT Trojan'"
+        ],
+        "NanoCore RAT Trojan": [
+            "product:'NanoCore RAT Trojan'"
+        ],
+        "Gh0st RAT Trojan": [
+            "product:'Gh0st RAT Trojan'"
+        ],
+        "DarkTrack RAT Trojan": [
+            "product:'DarkTrack RAT Trojan'"
+        ],
+        "njRAT Trojan": [
+            "product:'njRAT Trojan'"
+        ],
+        "Remcos Pro RAT Trojan": [
+            "product:'Remcos Pro RAT Trojan'"
+        ],
+        "Poison Ivy Trojan": [
+            "product:'Poison Ivy Trojan'"
+        ],
+        "Orcus RAT Trojan": [
+            "product:'Orcus RAT Trojan'"
+        ],
+        "Ares RAT C2": [
+            "product:'Ares RAT C2'"
+        ],
+        "ZeroAccess Trojan": [
+            "product:'ZeroAccess Trojan'"
+        ],
+        "Hookbot": [
+            "http.title:'Hookbot Panel'"
+        ],
+        # Credit: https://github.com/corumir
+        "Hak5 Cloud C2": [
+            "product:'Hak5 Cloud C2'",
+            "http.favicon.hash:1294130019"
+        ],
+        # Credit: https://github.com/corumir
+        # Tool: https://github.com/suriya73/BlackNET
+        "BlackNET Botnet": [
+            "http.title:'BlackNet - Login'"
+        ],
+        "Doxerina Botnet": [
+            "http.title:'Doxerina BotNet'"
+        ],
+        # Credit: https://github.com/corumir
+        # Tool: https://github.com/noke6262/RisePro-Stealer
+        "RisePro Stealer": [
+            "'Server: RisePro'"
+        ],
+        # Credit: https://github.com/corumir
+        # Tool: https://github.com/cassanof/pantegana
+        "Pantegana C2": [
+            "ssl:Pantegana ssl:localhost",
+            "ssl.cert.issuer.cn:'Pantegana Root CA'"
+        ],
+        # Credit: https://github.com/corumir
+        # Tool: https://github.com/tdragon6/Supershell/tree/main
+        "Supershell C2": [
+            "http.html_hash:84573275",
+            "http.favicon.hash:-1010228102",
+            "http.title:'Supershell - 登录'"
+        ],
+        "Viper C2": [
+            "http.html_hash:-1250764086"
+        ],
+        "Poseidon C2": [
+            "http.favicon.hash:219045137",
+            "http.html_hash:-1139460879",
+            "hash:799564296"
+        ],
+        "Scarab Botnet": [
+            "http.title:'Scarab Botnet PANEL'"
+        ],
+        "Bandit Stealer": [
+            "http.title:Login http.html:'Welcome to Bandit' 'Content-Length: 4125' port:8080"
+        ],
+        "NetBus Trojan": [
+            "product:'NetBus Trojan'"
+        ],
+        "UnamWebPanel": [
+            "html:unam_lib.js http.favicon.hash:-1278680098,-1531496738",
+            "http.title:'Unam Web Panel &mdash; Login'"
+        ],
+        "Atlandida Stealer": [
+            "http.title:'Atlantida' http.html:'GY7HXsD.jpg'"
+        ],
+        "Vshell C2": [
+            "http.title:'Vshell - 登录'"
+        ],
+        "BurpSuite": [
+            "product:BurpSuite"
+        ],
+        "Hachcat": [
+            "product:'Hachcat Cracking Tool'"
+        ],
+        "MobSF": [
+            "http.title:'Mobile Security Framework - MobSF'"
+        ],
+        "Villain C2": [
+            "hash:856668804"
+        ],
+        "SpyAgent": [
+            "http.title:'SpY-Agent v1.2'"
+        ]
     }
 
+    # https://www.techiedelight.com/delete-all-files-directory-python/
+    dir_to_clean = "data"
+    for file in os.scandir(dir_to_clean):
+        os.remove(file.path)
+
     ip_set_from_all_products = set()
-    count_of_all_ips = 0
-    count_of_products = 0
     for product in queries:
-        count_of_products += 1
-        count_of_product_ips = 0
         ip_set_from_product = set()
+        product_ips_file = open(f"data/{product} IPs.txt", "a")
         for query in queries[product]:
-            stdlog("Product: " + product + ", Query: " + query)
+            print(f"Product: {product}, Query: {query}")
+            results = api.search_cursor(query)
+            # Catch Shodan Query Errors and pass onto the next C2
+            # TODO: make it restart main() while keeping track of what was already documented
             try:
-                for result in api.search_cursor(query):
+                for result in results:
                     ip = str(result["ip_str"])
                     ip_set_from_product.add(ip)
                     ip_set_from_all_products.add(ip)
-            except APIError as error:
-                errlog(error)
-        product_ips_file = open(f"data/{product} IPs.txt", "w")
+            except exception.APIError:
+                continue
         for ip in ip_set_from_product:
             product_ips_file.write(f"{ip}\n")
-            count_of_product_ips += 1
-        product_ips_file.closed
-        stdlog("Created data/"+ product + " IPs.txt with " + str(count_of_product_ips) + " unique IP addresses from SHODAN")
 
     all_ips_file = open("data/all.txt", "a")
     for ip in ip_set_from_all_products:
         all_ips_file.write(f"{ip}\n")
-        count_of_all_ips += 1
-    stdlog("Created SHODAN all data with " + str(count_of_all_ips) +" unique IP addresses")
 
+def censys():
+    queries = {
+        "RisePro Stealer": [
+            "services.http.response.headers: (key: `Server` and value.headers: `RisePro`)",
+            "services.software.product:RisePro"
+        ],
+        "Viper C2": [
+            "services.software.product=`VIPER`"
+        ],
+        "Poseidon C2": [
+            "services.http.response.html_title=`POSEIDON`"
+        ],
+        "Scarab Botnet": [
+            "services.software.product=`Scarab`"
+        ],
+        "Cobalt Strike C2": [
+            "services.software.product=`Cobalt Strike`"
+        ],
+        "AsyncRAT": [
+            "services.software.product=`AsyncRAT`"
+        ],
+        "Supershell C2": [
+            "services.software.product=`Supershell`"
+        ],
+        "Hak5 Cloud C2": [
+            "services.software.product=`Cloud C2`"
+        ],
+        "Gh0st RAT Trojan": [
+            "services.service_name:`GHOST`"
+        ],
+        "DarkComet Trojan": [
+            "services.service_name:`DARKCOMET`"
+        ],
+        "DarkGate Loader": [
+            "services.service_name:`DARKGATE`"
+        ],
+        "Sliver C2": [
+            "services.software.product:`SLIVER`"
+        ],
+        "ShadowPad": [
+            "services.software.product:`SHADOWPAD`"
+        ],
+        "Mint Stealer": [
+            "services.software.product=`Mint Stealer`"
+        ],
+        "Mekotio Trojan": [
+            "services.software.product=`Mekotio`"
+        ],
+        "Gozi Trojan": [
+            "services.software.product=`Gozi`"
+        ],
+        "Browser Exploitation Framework (BeEF)": [
+            "services.software.product=`Browser Exploitation Framework (BeEF)`"
+        ],
+        "Mythic C2": [
+            "services.software.product:`Mythic`"
+        ],
+        "Vshell C2": [
+            "services.software.product=`Vshell`",
+            "services.http.response.html_title:'Vshell - 登录'"
+        ],
+        "Hookbot": [
+            "services.software.product:`Hookbot`"
+        ],
+        "Quasar RAT": [
+            "services.software.product:`Quasar`"
+        ],
+        "Havoc C2": [
+            "services.software.product:`Havoc`"
+        ],
+        "Atlandida Stealer": [
+            "services: (http.response.html_title:`Atlantida` and http.response.body:`GY7HXsD.jpg`)"
+        ],
+        "VenomRAT": [
+            "services.software.product:VenomRAT"
+        ],
+        "Orcus RAT": [
+            "services.software.product:Orcus"
+        ],
+        "DcRAT": [
+            "services.software.product:DcRat"
+        ],
+        "Posh C2": [
+            "services.software.product:PoshC2"
+        ],
+        "Deimos C2": [
+            "services.software.product:DeimosC2"
+        ],
+        "Covenant C2": [
+            "services.software.product:Covenant"
+        ],
+        "BitRAT": [
+            "services.software.product:BitRAT"
+        ],
+        "BlackDolphin": [
+            "services.software.product:BlackDolphin"
+        ],
+        "Artemis RAT": [
+            "services.software.product:'Artemis Rat'"
+        ],
+        "Godzilla Loader": [
+            "services.software.product:godzilla-loader"
+        ],
+        "Jinx Loader": [
+            "services.software.product:JinxLoader"
+        ],
+        "Neptune Loader": [
+            "services.software.product:neptune-loader"
+        ],
+        "NimPlant C2": [
+            "services.software.product:NimPlant"
+        ],
+        "Ares RAT C2": [
+            "services.software.product:'Ares RAT'"
+        ],
+        "Villain C2": [
+            "services.banner='whoami\\n'"
+        ],
+        "SpyAgent": [
+            "services.http.response.html_title:'SpY-Agent v1.2'",
+            "services.software.product=`Spy-Agent`"
+        ]
+    }
+    h = CensysHosts()
+    all_ips = set()
+    for product in queries:
+        ips = set()
+        product_ips_file = open(f"data/{product} IPs.txt", "a")
+        for search_string in queries[product]:
+            print(f"Product: {product}, Query: {search_string}")
+            query = h.search(search_string)
+            results = None
+            try:
+                results = query()
+            except Exception as err:
+                print(err)
+                continue
+            for host in results:
+                ip = str(host['ip'])
+                all_ips.add(ip)
+                ips.add(ip)
+        for ip in ips:
+            product_ips_file.write(f"{ip}\n")
+    all_ips_file = open("data/all.txt", "a")
+    for ip in all_ips:
+        all_ips_file.write(f"{ip}\n")
+
+def deconflict():
+    # Remove any duplicates from the files
+    files = os.listdir("data/")
+    for file in files:
+        filepath = f"data/{file}"
+        f = open(filepath, "r")
+        lines = f.readlines()
+        f.close()
+        if len(lines) != len(set(lines)):
+            print(f"Deconflicting: {filepath}")
+            os.remove(filepath)
+            f = open(filepath, "a")
+            for line in set(lines):
+                f.write(line)
+            f.close()
+
+def main():
+    load_dotenv()
+    shodan()
+    censys()
+    deconflict()
 
 if __name__ == '__main__':
-    print('')
-    print('░▒█▀▀▄░█▀█░░░░▀▀█▀▀░█▀▀▄░█▀▀▄░█▀▄░█░▄░█▀▀░█▀▀▄')
-    print('░▒█░░░░▒▄▀░▀▀░░▒█░░░█▄▄▀░█▄▄█░█░░░█▀▄░█▀▀░█▄▄▀')
-    print('░▒█▄▄▀░█▄▄░░░░░▒█░░░▀░▀▀░▀░░▀░▀▀▀░▀░▀░▀▀▀░▀░▀▀v1')
-    print('')
-    load_dotenv()
-    if 'SHODAN_API_KEY' not in os.environ:
-        errlog("The SHODAN_API_KEY environment variable is not defined.")
-        exit(1)  # Exit the script with a non-zero status code
-    # query Shodan
-    shodan()
+    main()
